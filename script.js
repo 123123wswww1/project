4d9dc41591323c640c32ff2bb7a4518bd7e5c2fe
Gleb <example@gmail.com>
2020-12-28 12:38:12 +0700

    Edit js code

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
 "use sctrict";
 
-// // const result = confirm("Are you here?");
-// // console.log(result);
-
-// const answer = prompt("Vam est' 18?", "");
-// console.log(typeof(answer));
-
-const category = 'toys';
-
-console.log('4' + " — object");
-
-let incr = 10,
-    decr = 10;
-
-// ++incr;
-// --decr;
-
-console.log(2 + (2 * 2) === 8);
-
-
-const isChecked = true, 
-      isClose = false;
-
-console.log(isChecked || isClose);
+let numberOfFilms = promt("Сколько фильмов вы уже посмотрели?", "");
+console.log(prompt);
