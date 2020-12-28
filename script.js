dc491e5b41f2928b2c0d61c48449e12a4133d788
Gleb <example@gmail.com>
2020-12-28 11:49:45 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+"use sctrict";
+
+// // const result = confirm("Are you here?");
+// // console.log(result);
+
+// const answer = prompt("Vam est' 18?", "");
+// console.log(typeof(answer));
+
+const category = 'toys';
+
+console.log('4' + " — object");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(2 + (2 * 2) === 8);
+
+
+const isChecked = true, 
+      isClose = false;
+
+console.log(isChecked || isClose);
