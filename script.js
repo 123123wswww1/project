446de3e0b70ee0e669a5610bfbd96c9ac89991fa
Gleb <example@gmail.com>
2020-12-28 13:29:50 +0700

    Practic №1

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 "use sctrict";
 
-let numberOfFilms = promt("Сколько фильмов вы уже посмотрели?", "");
-console.log(prompt);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+console.log(numberOfFilms);
+
+ const personalModaleDB = {
+    count: numberOfFilms,
+     movies: {},
+     actors: {},
+     genres: [],
+     private: false
+ };
+ 
+const a = prompt("Сколько фильмов вы уже посмотрели?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Сколько фильмов вы уже посмотрели?", ""),
+      d = prompt("На сколько оцените его?", "");
+    
+    
+personalModaleDB.movies[a] = b;
+personalModaleDB.movies[c] = d;
+
+
+console.log(personalModaleDB);
